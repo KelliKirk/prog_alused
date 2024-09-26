22e305084eb8b8cbd9b810c3551d004e7399b15e
Kelli Kirk <kelli.kirk@voco.ee>
2024-09-26 15:30:40 +0300

    Ylesanne 1.5 - lõpetatud

--- yl1.5.js
+++ yl1.5.js
@@ -3,10 +3,14 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-rl.question ('Sisestage ainepunktide arv: ', kolm_ainepunkti => {
+rl.question ('Sisestage ainepunktide arv: ', ainepunkt => {
 
 } )
 let ainepunkt = 26
-let kolm_ainepunkti = ainepunkt * 3
+let kolm_ainepunkti = ainepunkt *3
 let nadal = kolm_ainepunkti / 10
-rl.question ('Sisestage nädalate arv: ', )
+rl.question ('Sisestage nädalate arv: ', nadal => {
+
+} )
+console.log = (ainepunkt, nadal)
+rl.close()
