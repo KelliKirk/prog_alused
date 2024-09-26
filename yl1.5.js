b8c5e16fa94c57891bcaead922e19dff91950d72
Kelli Kirk <kelli.kirk@voco.ee>
2024-09-26 15:26:13 +0300

    Ylesanne 1.5 - poolik

---
+++ yl1.5.js
@@ -0,0 +1,12 @@
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question ('Sisestage ainepunktide arv: ', kolm_ainepunkti => {
+
+} )
+let ainepunkt = 26
+let kolm_ainepunkti = ainepunkt * 3
+let nadal = kolm_ainepunkti / 10
+rl.question ('Sisestage nädalate arv: ', )
