15f3666f54a3e7b16e55ff0925cf45f022e9f52e
Kelli Kirk <kelli.kirk@voco.ee>
2024-09-27 18:12:32 +0300

    Ylesanne 2.4 - lopetatud

--- yl2.4.js
+++ yl2.4.js
@@ -3,18 +3,30 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
- 
-rl.question('Kas soovite istekoha ise valida või loosida? ' , istekoht => {
-    if (istekoht == 'ise') {
-    console.log('Te valisite istekoha ise.')
-    console.log('Kas te soovite istuda akna ääres või mitte (muu)?')
-    console.log('Valisite ise: aknakoht')
-    console.log('Valisite ise: Vahekäigukoht')
-    console.log('Valisite ise: Keskmine koht')
-} else (istekoht == 'loos') {
-console.log(`Istekoht loositi: ${Math.random(1)*3} aken`)
-console.log(`Istekoht loositi: ${Math.random(1)*3} vahekäik`)
-console.log(`Istekoht loositi: ${Math.random(1)*3} keskel`) 
-} 
 
-})
+rl.question('Kas soovite istekoha ise valida või loosida? ', istekoht => {
+    if (istekoht === 'ise') {
+        rl.question('Kas te soovite istuda akna ääres (aken) või mujal (muu)? ', valik => {
+            if (valik === 'aken') {
+                console.log('Valisite ise: aknakoht');
+            } else if (valik === 'muu') {
+                console.log('Valisite ise: vahekäigukoht');
+            } else {
+                console.log('Vale sisend!');
+            }
+            rl.close();
+        });
+    } else if (istekoht === 'loos') {
+
+        let loos = Math.random(); 
+        if (loos < 1/3) {
+            console.log('Istekoht loositi: aknakoht');
+        } else {
+            console.log('Istekoht loositi: vahekäigukoht');
+        }
+        rl.close();
+    } else {
+        console.log('Vale sisend! Palun sisestage "ise" või "loos".');
+        rl.close();
+    }
+});
