fb7cb9c65e650b85b5f9ad589266a2ca94a6c9c0
Kelli Kirk <kelli.kirk@voco.ee>
2024-09-27 10:12:28 +0300

    Ylesanne 2.1 - parandatud

--- yl2.1.js
+++ yl2.1.js
@@ -3,10 +3,13 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-rl.question('Sisesta ohutemperatuur: ', temperatuur => { 
-    if (temperatuur) < 4.0 { 
+rl.question("Sisesta ohutemperatuur: ", temperatuur => { 
+    if (temperatuur < 4.0) { 
     console.log('On jaatumise oht')
     } else { 
         console.log('Ei ole jaatumise ohtu')
+      }  
     rl.close()
-})
+  })
+
+
